3294ff9d170e4d543e59d6d81175e672bf033b36
Anwesh43 <anweshthecool0@gmail.com>
2018-08-12 14:25:53 +0530

    adding PLCNode class

--- ProgressLineToCircleStage.ts
+++ ProgressLineToCircleStage.ts
@@ -77,3 +77,63 @@ class Animator {
         }
     }
 }
+
+class PLCNode {
+    state : State = new State()
+    next : PLCNode
+    prev : PLCNode
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new PLCNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = w / nodes
+        const sc1 : number = Math.min(0.5, this.state.scale) * 2
+        const sc2 : number = Math.min(0.5, Math.max(this.state.scale - 0.5, 0)) * 2
+        context.lineWidth = Math.min(w, h) / 60
+        context.lineCap = 'round'
+        context.strokeStyle = 'white'
+        context.save()
+        context.translate(this.i * gap + gap/2, h/2 + h/5 * sc2)
+        context.beginPath()
+        for(var j = 0; j <= 360; j++) {
+            const x : number = (gap/2) * Math.cos(j * Math.PI/180)
+            const y : number = (gap/2) * Math.sin(j * Math.PI/180)
+            if (j == 0) {
+                context.moveTo(x, y)
+            } else {
+                context.lineTo(x, y)
+            }
+        }
+        context.stroke()
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) {
+        var curr : PLCNode = this.prev
+        if (this.dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
